1094e58449ed8cec8142aa308db8184de6bc821c
subiranet <joan.joan425@gmail.com>
2025-04-24 18:58:58 +0200

    runner modified for training-testing

--- runner.py
+++ runner.py
@@ -7,6 +7,7 @@ import sys
 
 from train import TrainingSummarization
 from evaluate import EvaluationSummarizationPipeline
+from tester import TestModel
 
 logging.basicConfig(
     level=logging.INFO,
@@ -21,6 +22,7 @@ def main():
         # Initialize
         trainer = TrainingSummarization()
         evaluator = EvaluationSummarizationPipeline()
+        tester = TestModel()
 
         # Load data
         trainer.load_data()
@@ -35,20 +37,42 @@ def main():
         t_results = trainer.train()
 
         # Save model
-        trainer.save_model()
+        # trainer.save_model()
 
         # Configure evaluator
-        evaluator.model = trainer.model
-        evaluator.dataset = trainer.dataset['test']
+        # evaluator.model = trainer.model
+        # evaluator.dataset = trainer.dataset['test']
+
+        # Test results
+        logger.info("Loading data...")
+        tester.load_data()
+
+        logger.info("Loading model...")
+        tester.model = trainer.model
+
+        logger.info("Preparing dataset...")
+        tester.prepare_dataset()
+
+        logger.info("Generating summaries...")
+        summ = tester.generate_summaries()
+
+        logger.info("Evaluating test...")
+        tester.evaluate_test()
+
+        logger.info("Saving results...")
+        tester.save_results()  # Save both evaluation results and dataset
+
+        logger.info("Testing completed successfully!")
+
 
         # Evaluate model
-        e_results = evaluator.evaluate()
-
-        logger.info(f'\nTraining results for model {trainer.config['model']['name']}:')
-        logger.info(t_results)
-
-        logger.info(f'\nTraining results for model {evaluator.config['model']['name']}:')
-        logger.info(e_results)
+        # e_results = evaluator.evaluate()
+        #
+        # logger.info(f'\nTraining results for model {trainer.config['model']['name']}:')
+        # logger.info(t_results)
+        #
+        # logger.info(f'\nTraining results for model {evaluator.config['model']['name']}:')
+        # logger.info(e_results)
 
     except Exception as e:
         logger.error(f"Py runner execution failed: {str(e)}")
